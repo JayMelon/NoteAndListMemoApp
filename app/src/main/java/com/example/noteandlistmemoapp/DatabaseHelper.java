bdd4e7088c9b750535da40a08ad1514f250e59ea
Jackson Jones <jacksonkjones@protonmail.com>
2023-02-18 01:01:21 -0500

    Completed Note Class and DatabaseHelper Class

---
+++ app/src/main/java/com/example/noteandlistmemoapp/DatabaseHelper.java
@@ -0,0 +1,39 @@
+package com.example.noteandlistmemoapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "noteandlistmemos.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String NOTES_TABLE = "NOTES_TABLE";
+    public static final String ID = "COLUMN_NOTE_ID";
+    private static final String TITLE = "COLUMN_TITLE";
+    private static final String CONTENT = "COLUMN_CONTENT";
+    private static final String IMPORTANCE = "COLUMN_IMPORTANCE";
+    private static final String CREATION_TIME = "COLUMN_CREATION_TIME";
+
+    private static final String CREATE_TABLE = "CREATE TABLE " + NOTES_TABLE + " (" + ID +
+            " INTEGER PRIMARY KEY AUTOINCREMENT, " + TITLE + " TEXT, " + CONTENT + "TEXT, " +
+            IMPORTANCE + " TEXT, " + CREATION_TIME + " TEXT )";
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database) {
+        database.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase database, int i, int j) {
+        Log.w(DatabaseHelper.class.getName(), "Upgrading database from version " + i + " To" + j + ", which will destroy all old data");
+        database.execSQL("DROP TABLE IF EXISTS "+ TITLE);
+        onCreate(database);
+    }
+}
---
+++ app/src/main/java/com/example/noteandlistmemoapp/Note.java
@@ -0,0 +1,56 @@
+package com.example.noteandlistmemoapp;
+
+import java.util.Calendar;
+
+public class Note {
+    private int noteID;
+    private String title;
+    private String content;
+    private String importance;
+    private Calendar creationTime;
+
+    public Note() {
+        noteID = -1;
+        creationTime = Calendar.getInstance();
+    }
+
+    public int getNoteID() {
+        return noteID;
+    }
+
+    public void setNoteID(int noteID) {
+        this.noteID = noteID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImportance() {
+        return importance;
+    }
+
+    public void setImportance(String importance) {
+        this.importance = importance;
+    }
+
+    public Calendar getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(Calendar creationTime) {
+        this.creationTime = creationTime;
+    }
+}
