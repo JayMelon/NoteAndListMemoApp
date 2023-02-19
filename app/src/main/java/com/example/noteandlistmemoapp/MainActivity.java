698b0540dff51f80129de0f9ec06fe23c780f1b0
Jalen Lewis <jaylewis0618@gmail.com>
2023-02-18 23:10:27 -0500

    Added an Plus Icon to add a new note. Currently Toolbar is wip for compatibility

--- app/src/main/java/com/example/noteandlistmemoapp/MainActivity.java
+++ app/src/main/java/com/example/noteandlistmemoapp/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.noteandlistmemoapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
-
+Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
